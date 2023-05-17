925d91b9858df875de1ae4236dd2cd6e3939d2ab
BigIron0315 <132597510+BigIron0315@users.noreply.github.com>
2023-05-17 09:21:07 -0700

    Delete KDFL directory

--- KDFL/Teacher_model/__init__.py
+++
@@ -1,8 +0,0 @@
-from .TA_Resnet import resnet110, resnet110_Cifar100
-from .TA_Vgg import vgg19, vgg19_Cifar100
-model_dict1 = {
-    'TA_Resnet110_CIFAR10' : resnet110,
-    'TA_Resnet110_CIFAR100' : resnet110_Cifar100,
-    'TA_Vgg_CIFAR10' : vgg19,
-    'TA_Vgg_CIFAR100' : vgg19_Cifar100,
-}
